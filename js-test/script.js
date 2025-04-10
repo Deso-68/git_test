8d51d35332bf3d7563e51e45d14657de3c089d59
Deso-68 <174117432+Deso-68@users.noreply.github.com.>
2025-04-10 17:46:51 +0800

    feat: add initial HTML structure, JavaScript functionality, and CSS styles for shopping list app

---
+++ js-test/script.js
@@ -0,0 +1,15 @@
+const ulNode = document.querySelector("ul");
+const inputNode = document.querySelector("input");
+const btnNode = document.querySelector("button");
+
+btnNode.addEventListener("click", () => {
+        
+
+})
+
+const removeButton = document.querySelector("div");
+const button = document.querySelector("div button");
+
+removeButton.removeChild(button);
+console.log(document.all);
+console.log(document.URL);
